301a4462386ae9d70bf69051fa60379647e81f6a
Anthony Robin <contact@anthony-robin.fr>
2015-04-07 19:45:43 +0200

    Rubocop friendly

--- lib/awesome_share_buttons/helper.rb
+++ lib/awesome_share_buttons/helper.rb
@@ -5,18 +5,22 @@ module AwesomeShareButtons
       extra_data = {}
       rel = opts[:rel]
       html = []
-      html << "<div class='awesome-share-buttons' data-title='#{h title}' data-img='#{opts[:image]}' data-url='#{opts[:url]}' data-popup='#{opts[:popup]}'>"
+      html << "<div class='awesome-share-buttons' data-title='#{h title}' data-img='#{opts[:image]}' "
+      html << "data-url='#{opts[:url]}' data-desc='#{opts[:desc]}' data-popup='#{opts[:popup]}' data-via='#{opts[:via]}'>"
 
       AwesomeShareButtons.config.allow_sites.each do |name|
         extra_data = opts.select { |k, _| k.to_s.start_with?('data') } if name.eql?('tumblr')
 
-        link_title = t "awesome_share_buttons.share_to", :name => t("awesome_share_buttons.#{name.downcase}")
-        html << link_to("<i class='fa fa-#{get_icon(name.downcase)}'></i>".html_safe,"#", {rel: ['nofollow', rel],
-                                  "data-site" => name,
-                                  class: "awesome-share-buttons-#{name}",
-                                  onclick: "return AwesomeShareButtons.share(this);",
-                                  title: h(link_title)}.merge(extra_data))
+        link_title = t 'awesome_share_buttons.share_to', name: t("awesome_share_buttons.#{name.downcase}")
+        html << link_to("<i class='fa fa-#{get_icon(name.downcase)}'></i>".html_safe, '#', {
+          rel: ['nofollow', rel],
+          'data-site' => name,
+          class: "awesome-share-buttons-#{name}",
+          onclick: 'return AwesomeShareButtons.share(this);',
+          title: h(link_title)
+        }.merge(extra_data))
       end
+
       html << '</div>'
       raw html.join("\n")
     end
